eb14c843da8ed012d83e47acdafcc9ce942ea731
ninabtolo <145863239+ninabtolo@users.noreply.github.com>
2025-02-24 08:07:12 -0300

    Create Animal.java

---
+++ Engenharia 3/Strategy/Antipattern/Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    private String nome;
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void fazerSom() {
+        System.out.println(nome + " está fazendo um som.");
+    }
+}
